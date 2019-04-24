8189c2aa46fd5523d4d610d0a7341ddd9f58cad9
FromSi <fromsi665@gmail.com>
2019-04-24 21:52:51 +0000

    Added pywal

---
+++ arch-root/home/fromsi/.cache/wal/colors-wal-tabbed.h
@@ -0,0 +1,6 @@
+static const char* selbgcolor   = "#0e0f0f";
+static const char* selfgcolor   = "#dcc4a9";
+static const char* normbgcolor  = "#695146";
+static const char* normfgcolor  = "#dcc4a9";
+static const char* urgbgcolor   = "#6A5038";
+static const char* urgfgcolor   = "#dcc4a9";
